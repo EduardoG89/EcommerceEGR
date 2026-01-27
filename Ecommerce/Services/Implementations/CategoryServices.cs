fb3280d283a267c9282c400b29be215882a3df0d
edudev98 <eduardogalindo123ramirez@gmail.com>
2026-01-26 21:06:13 -0600

    Se añada clase para servicio categorias

---
+++ Ecommerce/Services/Implementations/CategoryServices.cs
@@ -0,0 +1,35 @@
+﻿using Ecommerce.Data;
+using Ecommerce.DTOs.Categories;
+using Ecommerce.Services.Interfaces;
+
+namespace Ecommerce.Services.Implementations
+{
+    public class CategoryServices : ICategoryService
+    {
+        private readonly AppDbContext _context;
+
+        public CategoryServices(AppDbContext context)
+        {
+            _context = context;
+        }
+        public Task<CategoryDto> CreateCategoryAsync(CreateCategoryDto createCategoryDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeleteCategoryAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<CategoryDto>> GetAllCategoriesAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<CategoryDto> GetCategoryByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
