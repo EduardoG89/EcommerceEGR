59a41833a584ad57d006b96cf909e7ebdbc72ed2
edudev98 <eduardogalindo123ramirez@gmail.com>
2026-02-10 21:41:19 -0600

    Se añade mas metodos del controlador de autenticación

--- Ecommerce/Controllers/AuthController.cs
+++ Ecommerce/Controllers/AuthController.cs
@@ -23,7 +23,7 @@ namespace Ecommerce.Controllers
             {
                 var result = await _authService.RegisterAsync(registerDto);
 
-                return Ok( new ApiResponse<AuthResponseDto>
+                return Ok(new ApiResponse<AuthResponseDto>
                 {
                     Success = true,
                     Message = "Usuario registrado exitosamente",
@@ -41,6 +41,58 @@ namespace Ecommerce.Controllers
                 });
             }
         }
+
+        [HttpPost("login")]
+        public async Task<ActionResult<ApiResponse<AuthResponseDto>>> Login([FromBody] LoginDto loginDto)
+        {
+            try
+            {
+                var result = await _authService.LoginAsync(loginDto);
+
+                return Ok(new ApiResponse<AuthResponseDto>
+                {
+                    Success = true,
+                    Message = "Login exitoso",
+                    Data = result
+                });
+            }
+            catch (Exception ex)
+            {
+
+                return BadRequest(new ApiResponse<AuthResponseDto>
+                {
+                    Success = false,
+                    Message = "Error al iniciar sesión",
+                    Errors = new List<string> { ex.Message }
+                });
+
+            }
+        }
+
+        [HttpGet("validate-email/{email}")]
+        public async Task<ActionResult<ApiResponse<bool>>> ValidateEmail(string email)
+        {
+            try
+            {
+                var result = await _authService.UserExistsAsync(email);
+
+                return Ok(new ApiResponse<bool>
+                {
+                    Success = true,
+                    Data = result
+                });
+            }
+            catch (Exception ex)
+            {
+
+                return BadRequest(new ApiResponse<bool>
+                {
+                    Success = false,
+                    Message = "Error al verificar email",
+                    Errors = new List<string>{ ex.Message }
+                });
+            }
+        } 
 
     }
 }
