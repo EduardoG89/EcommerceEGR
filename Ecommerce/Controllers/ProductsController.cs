7530be662515e3d3038fce42b73f6ddf472c62b3
edudev98 <eduardogalindo123ramirez@gmail.com>
2026-02-14 21:39:36 -0600

    se añaden metodos productos

--- Ecommerce/Controllers/ProductsController.cs
+++ Ecommerce/Controllers/ProductsController.cs
@@ -1,5 +1,6 @@
 ﻿using Ecommerce.DTOs.Common;
 using Ecommerce.DTOs.Products;
+using Ecommerce.Models;
 using Ecommerce.Services.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 
@@ -16,18 +17,48 @@ namespace Ecommerce.Controllers
             _productServices = productService;
         }
 
-        [HttpGet]
+
+        [HttpPost("create-product")]
+        public async Task<ActionResult<ApiResponse<ProductDto>>> CreateProduct([FromBody] CreateProductDto createProductDto)
+        {
+            try
+            {
+                var product = await _productServices.CreateProductAsync(createProductDto);
+
+                return CreatedAtAction(
+                nameof(GetProductById),
+                new { id = product.Id },
+                new ApiResponse<ProductDto>
+                {
+                    Success = true,
+                    Data = product,
+                    Message = "Producto creado exitosamente"
+                });
+            }
+            catch (Exception ex)
+            {
+
+                return BadRequest(new ApiResponse<ProductDto>
+                {
+                    Success = false,
+                    Message = "Error al crear producto",
+                    Errors = new List<string> { ex.Message }
+                });
+            }
+        }
+
+        [HttpGet("all-products")]
         public async Task<ActionResult<ApiResponse<List<ProductDto>>>> GetAllProducts()
         {
 
             try
             {
-                var result = await _productServices.GetAllProductsAsync();
+                var product = await _productServices.GetAllProductsAsync();
 
                 return Ok(new ApiResponse<List<ProductDto>>
                 {
                     Success = true,
-                    Data = result
+                    Data = product
                 });
             }
             catch (Exception ex)
@@ -36,11 +67,37 @@ namespace Ecommerce.Controllers
                 return BadRequest(new ApiResponse<List<ProductDto>>
                 {
                     Success = false,
-                    Data = [],
-                    Message = ex.Message
+                    Message = "Error al obtener todos los productos",
+                    Errors = new List<string> { ex.Message }
                 });
             }
 
+        }
+
+        [HttpGet("product-by-id/{id}")]
+        public async Task<ActionResult<ApiResponse<ProductDto>>> GetProductById(int id)
+        {
+            try
+            {
+                var product = await _productServices.GetProductByIdAsync(id);
+
+                return Ok(new ApiResponse<ProductDto>
+                {
+
+                    Success = true,
+                    Data = product
+                });
+            }
+            catch (Exception ex)
+            {
+
+                return BadRequest(new ApiResponse<ProductDto>
+                {
+                    Success = false,
+                    Message = "Producto no encontrado",
+                    Errors = new List<string> { ex.Message }
+                });
+            }
         }
     }
 }
