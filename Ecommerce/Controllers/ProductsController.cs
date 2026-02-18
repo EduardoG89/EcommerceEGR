e2792439f593539791b5fd664bcb0e1a097ac889
edudev98 <eduardogalindo123ramirez@gmail.com>
2026-02-17 21:58:37 -0600

    se añaden cambios completos para controlador de productos

--- Ecommerce/Controllers/ProductsController.cs
+++ Ecommerce/Controllers/ProductsController.cs
@@ -18,7 +18,7 @@ namespace Ecommerce.Controllers
         }
 
 
-        [HttpPost("create-product")]
+        [HttpPost]
         public async Task<ActionResult<ApiResponse<ProductDto>>> CreateProduct([FromBody] CreateProductDto createProductDto)
         {
             try
@@ -44,10 +44,11 @@ namespace Ecommerce.Controllers
                     Message = "Error al crear producto",
                     Errors = new List<string> { ex.Message }
                 });
+
             }
         }
 
-        [HttpGet("all-products")]
+        [HttpGet]
         public async Task<ActionResult<ApiResponse<List<ProductDto>>>> GetAllProducts()
         {
 
@@ -74,7 +75,7 @@ namespace Ecommerce.Controllers
 
         }
 
-        [HttpGet("product-by-id/{id}")]
+        [HttpGet("{id}")]
         public async Task<ActionResult<ApiResponse<ProductDto>>> GetProductById(int id)
         {
             try
@@ -95,6 +96,143 @@ namespace Ecommerce.Controllers
                 {
                     Success = false,
                     Message = "Producto no encontrado",
+                    Errors = new List<string> { ex.Message }
+                });
+            }
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<ApiResponse<ProductDto>>> UpdateProduct(int id, [FromBody] UpdateProductDto updateProductDto)
+        {
+            try
+            {
+                if (id != updateProductDto.Id)
+                {
+                    return BadRequest(new ApiResponse<ProductDto>
+                    {
+                        Success = false,
+                        Message = "Id no coincide"
+                    });
+                }
+
+                var product = await _productServices.UpdateProductAsync(updateProductDto);
+
+                return Ok(new ApiResponse<ProductDto>
+                {
+                    Success = true,
+                    Data = product,
+                    Message = "Producto actualizado correctamente"
+                });
+            }
+            catch (Exception ex)
+            {
+
+                return BadRequest(new ApiResponse<ProductDto>
+                {
+                    Success = false,
+                    Message = "Error al actualizar el producto",
+                    Errors = new List<string> { ex.Message }
+                });
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<ApiResponse<ProductDto>>> DeleteProduct(int id)
+        {
+            try
+            {
+                var product = await _productServices.DeleteProductAsync(id);
+
+                return Ok(new ApiResponse<bool>
+                {
+                    Success = true,
+                    Message = "Producto eliminado exitosamente"
+                });
+            }
+            catch (Exception ex)
+            {
+
+                return BadRequest(new ApiResponse<bool>
+                {
+                    Success = false,
+                    Message = "Error al eliminar el producto",
+                    Errors = new List<string> { ex.Message }
+                });
+            }
+        }
+
+        [HttpGet("category/{id}")]
+        public async Task<ActionResult<ApiResponse<List<ProductDto>>>> GetProductByCategory(int id)
+        {
+            try
+            {
+                var products = await _productServices.GetProductsByCategoryAsync(id);
+
+                return Ok(new ApiResponse<List<ProductDto>>
+                {
+                    Success = true,
+                    Data = products
+                });
+            }
+            catch (Exception ex)
+            {
+
+                return BadRequest(new ApiResponse<List<Product>>
+                {
+                    Success = false,
+                    Message = "Error al obtener productos por categoria",
+                    Errors = new List<string> { ex.Message }
+                });
+            }
+        }
+
+        [HttpPost("variants")]
+        public async Task<ActionResult<ApiResponse<ProductVariantDto>>> AddVariant([FromBody] CreateProductVariantDto createProductVariantDto)
+        {
+            try
+            {
+                var variant = await _productServices.AddVariantAsync(createProductVariantDto);
+
+                return Ok(new ApiResponse<ProductVariantDto>
+                {
+                    Success = true,
+                    Message = "Variante creado exitosamente",
+                    Data = variant
+                });
+            }
+            catch (Exception ex)
+            {
+
+                return BadRequest(new ApiResponse<ProductVariantDto>
+                {
+                    Success = false,
+                    Message = "Error al crear la variante",
+                    Errors = new List<string> { ex.Message }
+                });
+            }
+        }
+
+        [HttpPatch("variant/{variantId}/stock")]
+        public async Task<ActionResult<ApiResponse<bool>>> UpdateStock(int variantId, [FromBody] int quantity)
+        {
+            try
+            {
+                var result = await _productServices.UpdateStockAsync(variantId, quantity);
+
+                return Ok(new ApiResponse<bool>
+                {
+                    Success = true,
+                    Message = "Stock actualizado exitosamente",
+                    Data = result
+                });
+            }
+            catch (Exception ex)
+            {
+
+                return BadRequest(new ApiResponse<bool>
+                {
+                    Success = false,
+                    Message = "Error al actualizar stock",
                     Errors = new List<string> { ex.Message }
                 });
             }
