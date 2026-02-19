c09b5d40bac5bcd2fc58b383d3b210f7c4c36fd6
edudev98 <eduardogalindo123ramirez@gmail.com>
2026-02-18 22:14:07 -0600

    se añade controlador orden

---
+++ Ecommerce/Controllers/OrderController.cs
@@ -0,0 +1,76 @@
+﻿using Ecommerce.DTOs.Common;
+using Ecommerce.DTOs.Orders;
+using Ecommerce.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ecommerce.Controllers
+{
+    [Route("api/[Controller]")]
+    [ApiController]
+    public class OrderController : ControllerBase
+    {
+        private readonly IOrderService _orderService;
+
+        public OrderController(IOrderService orderService)
+        {
+            _orderService = orderService;
+        }
+
+
+        [HttpPost]
+        public async Task<ActionResult<ApiResponse<OrderDto>>> CreateOrder([FromBody] CreateOrderDto createOrderDto)
+        {
+            try
+            {
+                var order = await _orderService.CreateOrderAsync(createOrderDto);
+
+                return CreatedAtAction(
+                    nameof(GetOrderById),
+                    new { id = order.Id },
+                    new ApiResponse<OrderDto>
+                    {
+                        Success = true,
+                        Data = order,
+                        Message = "Orden creado exitosamente"
+                    });
+            }
+            catch (Exception ex)
+            {
+
+                return BadRequest(new ApiResponse<OrderDto>
+                {
+                    Success = false,
+                    Message = "Error al crear la orden",
+                    Errors = new List<string> { ex.Message }
+                });
+            }
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<ApiResponse<OrderDto>>> GetOrderById(int id)
+        {
+            try
+            {
+                var order = await _orderService.GetOrderByIdAsync(id);
+
+                return Ok(new ApiResponse<OrderDto>
+                {
+                    Success = true,
+                    Data = order,
+                });
+
+            }
+            catch (Exception ex)
+            {
+
+                return BadRequest(new ApiResponse<OrderDto>
+                {
+                    Success = false,
+                    Message = "Error al obtener el orden",
+                    Errors = new List<string> { ex.Message }
+                });
+            }
+
+        }
+    }
+}
